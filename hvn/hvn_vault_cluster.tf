23be622c508811bd319444760b0536efdf844d37
jeremymyers <jeremy@jeremy.com>
2024-08-05 22:14:16 -0600

    setup

---
+++ hvn/hvn_vault_cluster.tf
@@ -0,0 +1,23 @@
+resource "hcp_hvn" "hashiconf_hvn" {
+  hvn_id         = "hashiconf24"
+  cloud_provider = "azure"
+  region         = "eastus"
+  cidr_block     = "172.25.16.0/20"
+}
+
+## terraform import hcp_hvn.hashiconf_hvn hashiconf22hvn
+
+resource "hcp_vault_cluster" "hashiconf24" {
+  cluster_id      = "azure-vault-cluster"
+  hvn_id          = hcp_hvn.hashiconf_hvn.hvn_id
+  tier            = "starter_small"
+  public_endpoint = true
+
+  ip_allowlist {
+    address = "76.131.179.73"
+    description = "my public IP"
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
---
+++ hvn/variables.tf
@@ -0,0 +1,9 @@
+variable "hvn_client_id" {
+  type = string
+  description = "hvn client id for hashiconf24"
+}
+
+variable "hvn_client_secret" {
+  type = string
+  description = "hvn client secret for hashiconf24"
+}
